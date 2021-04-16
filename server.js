ae27b8d7e65654c94fb269f2dce177570ab9f1ad
mathusihan <mathusihan@gmail.com>
2021-04-16 01:28:11 -0400

    Added login in back end

--- server.js
+++ server.js
@@ -3,6 +3,72 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://Mathusihan:CPS831@cluster0.tnbgf.mongodb.net/CPS831ASSIGNMENT?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+var currentUser;
+var conversations = [];
+
+class User {
+
+    constructor(username, email, password, firends, pendingFriends) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.friends = firends;
+        this.pendingFriends = pendingFriends;
+    }
+
+}
+
+class Conversation {
+
+    constructor(usernames, messages) {
+        this.usernames = usernames;
+        this.messages = messages;
+        this.messages.sort(function(a, b) { return b.timestamp - a.timestamp }); // Sort newest first
+    }
+
+}
+
+class ReturnObj {
+    constructor(currentUser, conversation) {
+        this.currentUser = currentUser;
+        this.conversation = conversation;
+    }
+}
+
+
+async function getUser(username, password) {
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        var UserList = client.db("CPS831ASSIGNMENT").collection("UserList");
+        var MessageList = client.db("CPS831ASSIGNMENT").collection("MessageList");
+        var query = { username: username };
+        currentUser = await UserList.findOne(query);
+        if (currentUser != undefined && currentUser != null && password == currentUser.password) {
+            query = { usernames: { $all: [username] } };
+            var messasgesArray = await MessageList.find(query).toArray();
+            messasgesArray.forEach(element => conversations.push(new Conversation(element.usernames, element.messages)));
+            return new ReturnObj(currentUser, messasgesArray);
+        } else {
+            return null;
+        }
+
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+    /*finally {
+           await client.close();
+       }*/
+}
+
+
+async function close() {
+    await client.close();
+}
 
 app.use(express.static(__dirname));
 
@@ -12,9 +78,25 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('login', function(username, password) {
+        (async function() {
+            var returnobj = null;
+            returnobj = await getUser(username, password);
+            if (returnobj != null && returnobj != undefined) {
+                io.emit('login_successful', returnobj.currentUser, returnobj.conversation);
+            } else
+                io.emit('login_fail');
+        })();
+
+    });
+
+
     socket.on('chat_message', function(message) {
-        console.log('test');
         io.emit('chat_message', '<strong>' + "socket.username" + '</strong>: ' + message);
+    });
+
+    socket.on('close_client', function() {
+        close();
     });
 });
 
